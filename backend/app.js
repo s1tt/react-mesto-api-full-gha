366310ba335083744a81abb80b400596c9578a30
Alekseev Sergei <nec13@mail.ru>
2023-08-18 03:46:38 +0300

    PR 15

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
-app.use(cors());
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
